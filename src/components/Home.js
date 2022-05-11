7980ee07996c91c7a409db3fa2a314cf3a048df6
luiz <lguilhermefl@gmail.com>
2022-05-11 13:22:21 -0300

    adicionada homepage

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+    return (
+        <div className="home">
+            <img src='img/logo.png' alt='Logo' />
+            <h1>ZapRecall</h1>
+            <button>Iniciar Recall!</button>
+        </div>
+    );
+}
